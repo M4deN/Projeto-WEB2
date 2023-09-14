57a2db01a2a6afeb9cbb428fd48b742f19c45096
Alecio L. Medeiros <Alexdesaran@outlook.com>
2023-09-14 09:40:46 -0300

    add wait

--- cypress/integration/Projeto_WEB/Livros.js
+++ cypress/integration/Projeto_WEB/Livros.js
@@ -1,16 +1,13 @@
 describe('Página de Livros', () => {
   before(() => {
-    cy.timeout(6000)
     cy.visit('http://localhost:3000/livros');
   });
 
   it('Ordenar livros por autor de forma ascendente', () => {
-    cy.timeout(6000)
     cy.get('#order-author').select('A-Z')
   });
   
   it('Buscar livros por título', () => {
-    cy.timeout(6000)
     cy.get('#search-input').type('Livro 1');  
     cy.get('#search-button').click();  
     cy.get('#livros-lista li').should('have.length', 6);
